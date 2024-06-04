6739ed125fdee229c64ddf77b9f0baf9e89bb459
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2024-06-03 19:48:13 -0500

    add: fonts config

---
+++ public/assets/fonts/index.ts
@@ -0,0 +1,15 @@
+import localFont from 'next/font/local'
+
+// export const InterFont = localFont({
+//   src: '/assets/fonts/Inter/Inter-Black.ttf',
+//   // display: 'swap',
+//   variable: '--special-font', // Optional, for CSS variable
+// })
+
+export const MonospaceKrypton = localFont({
+  src: './MonospaceKrypton/MonospaceKrypton-Bold.otf',
+})
+
+export const Inter = localFont({
+  src: './Inter/Inter-Regular.ttf',
+})
